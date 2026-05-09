b508a29ab7c4b40ec1a3054af1e7688e9806e9d7
Manishpratapsingh23 <manishpratap2205@gmail.com>
2026-05-09 13:00:13 +0530

    fixed login toaster

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <Toaster position="top-right" />
+      <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="flex justify-center text-[var(--color-primary)]">
           <BsBuildingFill size={48} />
